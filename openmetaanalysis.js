f81e31c5a7eedd1e36d61a02339eea060af7fba5
badgettrg <rbadgett@kumc.edu>
2014-11-22 11:55:57 -0600

    Update openmetaanalysis.js

--- openmetaanalysis.js
+++ openmetaanalysis.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $(".main").click(function(event){
     event.preventDefault();
 	window.location.href = "http://openmetaanalysis.github.io/" + $(this).attr("href")
+  });
   $(".repo").click(function(event){
     event.preventDefault();
 	window.location.href = "http://github.com/" + $(this).attr("href")
