ade0fc0f292d88d8dee75cebce326b79094e6caf
badgettrg <rbadgett@kumc.edu>
2014-11-22 11:38:40 -0600

    Update openmetaanalysis.js

--- openmetaanalysis.js
+++ openmetaanalysis.js
@@ -7,4 +7,8 @@ $(document).ready(function(){
     event.preventDefault();
 	window.location.href = "http://github.com/" + $(this).attr("href")
   });
+  $(".pmid").click(function(event){
+    event.preventDefault();
+	window.location.href = "http://www.ncbi.nlm.nih.gov/pubmed/" + $(this).text()
+  });
 })
