d62529ff117c8bd2157ee8227b46cf4cd36e9fc8
badgettrg <rbadgett@kumc.edu>
2014-11-22 11:32:27 -0600

    Update and rename tables-colors.js to tables.js

--- tables-colors.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $( "td:contains('Unclear')" ).css('background-color', '#ffcccc' );
-    $( "td:contains('High')" ).css('background-color', '#ff5959' );
-    $( "tr:contains('Unclear')").find('td:first').css('background-color', '#ffcccc' );
-    $( "tr:contains('High')").find('td:first').css('background-color', '#ff5959' );
-    $( ".emphasis" ).css('background-color', '#FFFF00' );
-    $( ".emphasis" ).css('font-style', 'italic' );
-    $( ".emphasis" ).css('font-weight', 'bold' );
-});
---
+++ tables.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $( "td:contains('Unclear')" ).css('background-color', '#ffcccc' );
+    $( "td:contains('High')" ).css('background-color', '#ff5959' );
+    $( "tr:contains('Unclear')").find('td:first').css('background-color', '#ffcccc' );
+    $( "tr:contains('High')").find('td:first').css('background-color', '#ff5959' );
+
+    $( ".emphasis" ).css('background-color', '#FFFF00' );
+    $( ".emphasis" ).css('font-style', 'italic' );
+    $( ".emphasis" ).css('font-weight', 'bold' );
+
+    $("#header_pico").html("<div class='header_panel'><p>Included trials, starting with the oldest trials.</p><table><caption>Randomized controlled trials of this topic</caption><tbody><tr><th>Trial</th><th>Patients</th><th>Intervention</th><th>Comparison</th><th>Outcome</th><th style='width:7px;background-color:white;border: 1px solid white'></th></tr></table>");
+    $("#header_bias").html("<p>This assessment is done with the Cochrane Collaboration's tool for assessing risk of bias (see '<a href='https://github.com/openMetaAnalysis/_Methods/blob/master/README.md' target='_blank'>Criteria for judging risk of bias</a>' in the 'Risk of bias' assessment tool').</p><p>Included trials, starting with the oldest trials.</p><table><caption>Possible bias in randomized controlled trials of this topic</caption><tr><th class='col1' rowspan='2'>Trial</th><th colspan='2'>Selection bias</th><th>Performance bias</th><th>Detection bias</th><th>Attrition bias</th><th>Reporting bias</th><th>Other bias</th><th rowspan=2 style='width:7px;background-color:white;border: 1px solid white'></th></tr><tr><th>Random sequence generation</th><th>Allocation concealment</th><th>Blinding of participants and personnel</th><th>Blinding of outcome assessment</th><th>Incomplete outcome data</th><th>Selective reporting</th><th>Other sources of bias</th></tr></table>");
+});
