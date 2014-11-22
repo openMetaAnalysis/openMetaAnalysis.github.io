49a863c245f4e833f6124fdcbd3e58f077239cd6
badgettrg <rbadgett@kumc.edu>
2014-11-22 09:40:03 -0600

    Update and rename tables-scrolling.js to tables-colors.js

--- tables-colors.js
+++ tables-colors.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
     $( "td:contains('Unclear')" ).css('background-color', '#ffcccc' );
-});
-$(document).ready(function(){
     $( "td:contains('High')" ).css('background-color', '#ff5959' );
-});
-$(document).ready(function(){
     $( "tr:contains('Unclear')").find('td:first').css('background-color', '#ffcccc' );
-});
-$(document).ready(function(){
     $( "tr:contains('High')").find('td:first').css('background-color', '#ff5959' );
+    $( ".emphasis" ).css('background-color', '#FFFF00' );
+    $( ".emphasis" ).css('font-style', 'italic' );
+    $( ".emphasis" ).css('font-weight', 'bold' );
 });
