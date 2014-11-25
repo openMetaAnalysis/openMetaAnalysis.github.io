c6880b5daeee914680861f06e1e97ec6205e9ed8
badgettrg <rbadgett@kumc.edu>
2014-11-25 08:11:26 -0600

    Update openmetaanalysis.js

--- openmetaanalysis.js
+++ openmetaanalysis.js
@@ -1,6 +1,13 @@
 var repo_dir = location.pathname.substring(1,location.pathname.indexOf("/",1));
 var repo_name = repo_dir.replace('-', ' ');
 $(document).ready(function(){
+	//Highlight emphasis
+	$( ".emphasis" ).css('background-color', '#FFFF00' );
+	$( ".emphasis" ).css('font-style', 'italic' );
+	$( ".emphasis" ).css('font-weight', 'bold' );
+	$( "emphasis" ).css('background-color', '#FFFF00' );
+	$( "emphasis" ).css('font-style', 'italic' );
+	$( "emphasis" ).css('font-weight', 'bold' );
 	//Display the repo_name in all the correct spots
 	$(".repo_name").text(repo_name);
 	//Link handlers
