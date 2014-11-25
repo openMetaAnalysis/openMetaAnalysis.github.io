d2264cfdd07b9efc4d76e6849cde85a723629d5e
badgettrg <rbadgett@kumc.edu>
2014-11-24 22:45:57 -0600

    Update openmetaanalysis.js

--- openmetaanalysis.js
+++ openmetaanalysis.js
@@ -1,17 +1,19 @@
-  var repo_dir = location.pathname.substring(1,location.pathname.indexOf("/",1));
-  var repo_name = repo_dir.replace('-', ' ');
 $(document).ready(function(){
-  //Link handlers
-  $(".main").click(function(event){
-    event.preventDefault();
-	window.location.href = "http://openmetaanalysis.github.io/" + $(this).attr("href")
-  });
-  $(".repo").click(function(event){
-    event.preventDefault();
-	window.location.href = "http://github.com/" + $(this).attr("href")
-  });
-  $(".pmid").click(function(event){
-    event.preventDefault();
-	window.location.href = "http://www.ncbi.nlm.nih.gov/pubmed/" + $(this).text() //$(this).attr("href")
-  });
+	var repo_dir = location.pathname.substring(1,location.pathname.indexOf("/",1));
+	var repo_name = repo_dir.replace('-', ' ');
+	//Display the repo_name in all the correct spots
+	$(".repo_name").text(repo_name);
+	//Link handlers
+	$(".main").click(function(event){
+		event.preventDefault();
+		window.location.href = "http://openmetaanalysis.github.io/" + $(this).attr("href");
+	});
+	$(".repo").click(function(event){
+		event.preventDefault();
+		window.location.href = "http://github.com/" + $(this).attr("href");
+	});
+	$(".pmid").click(function(event){
+		event.preventDefault();
+		window.location.href = "http://www.ncbi.nlm.nih.gov/pubmed/" + $(this).text(); //$(this).attr("href")
+	});
 })
