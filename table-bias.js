dea5186a5c004930b6c6bf481894adb8fb6fff29
badgettrg <rbadgett@kumc.edu>
2014-11-25 20:16:42 -0600

    Update table-bias.js

--- table-bias.js
+++ table-bias.js
@@ -1,3 +1,10 @@
+$( document ).ajaxComplete(function() {
+	//Background-color of cells
+	$( "td:contains('Unclear')" ).css('background-color', '#ffcccc' );
+	$( "td:contains('High')" ).css('background-color', '#ff5959' );
+	$( "tr:contains('Unclear')").find('td:first').css('background-color', '#ffcccc' );
+	$( "tr:contains('High')").find('td:first').css('background-color', '#ff5959' );
+});
 $(document).ready(function(){
 $("#header_bias").html("<p>This assessment is done with the Cochrane Collaboration's tool for assessing risk of bias (see '<a href='https://github.com/openMetaAnalysis/_Methods/blob/master/README.md' target='_blank'>Criteria for judging risk of bias</a>' in the 'Risk of bias' assessment tool').</p><p>Included trials, starting with the oldest trials.</p><table><caption>Possible bias in randomized controlled trials of this topic</caption><tr><th class='col1' rowspan='2'>Trial</th><th colspan='2'>Selection bias</th><th>Performance bias</th><th>Detection bias</th><th>Attrition bias</th><th>Reporting bias</th><th>Other biased</th><th rowspan=2 style='width:7px;background-color:white;border: 1px solid white'></th></tr><tr><th>Random sequence generation</th><th>Allocation concealment</th><th>Blinding of participants and personnel</th><th>Blinding of outcome assessment</th><th>Incomplete outcome data</th><th>Selective reporting</th><th>E.g. imbalanced compliance , co-interventions, or other.</th></tr></table>");
 var url = "/template/tables/bias.xml";
