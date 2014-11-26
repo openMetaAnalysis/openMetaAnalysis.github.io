ad54a47443c6d15365f9143b77f5563ffce93729
badgettrg <rbadgett@kumc.edu>
2014-11-26 08:37:19 -0600

    Update table-pico.js

--- table-pico.js
+++ table-pico.js
@@ -33,9 +33,12 @@ var url = "/" + repo_dir + "/tables/pico.xml";
 						$(this).find("bullet[type='primary']").each(function(){
 							outcome += '<br>&bull; ' + $(this).html()
 						})
-						$(this).find("bullet[type='secondary']").each(function(){
-							outcome += '<br>&bull; ' + $(this).html()
-						})
+						if ($(this).find("bullet[type='secondary']").length) {
+							outcome += 'Secondary:';
+							$(this).find("bullet[type='secondary']").each(function(){
+								outcome += '<br>&bull; ' + $(this).html()
+							})
+						}
 					})
                         	var pmid= $(this).find('citation').attr('pmid');
 				var trHTML = '<tr><td>' + $(this).find('citation').text() + ', ' + $(this).find("citation").attr("year") +  '<br>' + $(this).find("citation").attr("journal_abbrev") + "<br>PMID: <a href='http://pubmed.gov/" + pmid + "'>" + pmid + '</td><td>' + patients + '</td><td>' + intervention + '</td><td>' + comparison + '</td><td>' + outcome + '</td></tr>';
