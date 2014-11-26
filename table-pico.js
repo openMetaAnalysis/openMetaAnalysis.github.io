afa1d5bb7b8610251cb9192729c7ca2aa6c5cde4
badgettrg <rbadgett@kumc.edu>
2014-11-26 08:59:35 -0600

    Update table-pico.js

--- table-pico.js
+++ table-pico.js
@@ -16,7 +16,7 @@ var url = "/" + repo_dir + "/tables/pico.xml";
 			$(xml).find('study').each(function(){
 				var patients = $(this).find('patients').attr('total') + ' patients:';
 					$(this).find('patients').find('bullet').each(function(){
-						patients += '<br>&bull; ' + $(this)
+						patients += '<br>&bull; ' + $(this).html()
 						})
 				var intervention = '';
 					$(this).find('intervention').find('bullet').each(function(){
