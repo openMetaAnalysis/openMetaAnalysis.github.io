16378d18627389f3dadf2fec378cd8c2ec5f658c
badgettrg <rbadgett@kumc.edu>
2014-11-26 08:27:09 -0600

    Update table-pico.js

--- table-pico.js
+++ table-pico.js
@@ -28,7 +28,7 @@ var url = "/" + repo_dir + "/tables/pico.xml";
 						comparison += '<br>&bull; ' + $(this).html()
 						})
 				comparison = $(this).find('comparison').find('bullet').remove().end().html() + comparison
-				var outcome = 'Primary';
+				var outcome = 'Primary:';
 					$(this).find('outcome').find("bullet[type='primary']").each(function(){
 						outcome += '<br>&bull; ' + $(this).html()
 						$("[type=primary]").each(function(){
@@ -39,6 +39,10 @@ var url = "/" + repo_dir + "/tables/pico.xml";
 							outcome += '<br>&bull; ' + $(this).html()
 						})
 					})
+					if ($(this).find('outcome').find("bullet[type='secondary']").length){
+						outcome += 'Secondary:';
+						}
+					}
                         	var pmid= $(this).find('citation').attr('pmid');
 				var trHTML = '<tr><td>' + $(this).find('citation').text() + ', ' + $(this).find("citation").attr("year") +  '<br>' + $(this).find("citation").attr("journal_abbrev") + "<br>PMID: <a href='http://pubmed.gov/" + pmid + "'>" + pmid + '</td><td>' + patients + '</td><td>' + intervention + '</td><td>' + comparison + '</td><td>' + outcome + '</td></tr>';
 			        $('#citations').append(trHTML);
