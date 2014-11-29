9f33f802bf93714ab7af46a843a699e0c5d55d4b
badgettrg <rbadgett@kumc.edu>
2014-11-29 16:19:07 -0600

    Update openmetaanalysis.js

--- openmetaanalysis.js
+++ openmetaanalysis.js
@@ -2,20 +2,11 @@ var n = location.pathname.indexOf("/",1)
 if (n == 0){n = location.pathname.length}
 var repo_dir = location.pathname.substring(1,n);
 var repo_name = repo_dir.replace(/\-/gi, ' '); 
-$( document ).ajaxComplete(function() {
+$(document).ready(function(){
 	//Display the repo_name in all the correct spots
 	$(".repo_name").text(repo_name);
-	//Highlight emphasis
-	$( ".emphasis" ).css('background-color', '#FFFF00' );
-	$( ".emphasis" ).css('font-style', 'italic' );
-	$( ".emphasis" ).css('font-weight', 'bold' );
-	$( "emphasis" ).css('background-color', '#FFFF00' );
-	$( "emphasis" ).css('font-style', 'italic' );
-	$( "emphasis" ).css('font-weight', 'bold' );
-	});
-$(document).ready(function(){
 	//Customize src for images based on repo name
-	$("img.master").prop('src', 'https://raw.githubusercontent.com/openMetaAnalysis/' + repo_dir + '/master/' + $("img.master").prop('src'))
+	$("img .master").prop('src', 'https://raw.githubusercontent.com/openMetaAnalysis/' + repo_dir + '/master/' + $("img.master").prop('src'))
 	//Link handlers
 	$(".main").click(function(event){
 		event.preventDefault();
