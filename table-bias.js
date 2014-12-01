feed0c88924575047d410e320e88680a1dc9bd95
badgettrg <rbadgett@kumc.edu>
2014-12-01 00:17:59 -0600

    Update table-bias.js

--- table-bias.js
+++ table-bias.js
@@ -4,6 +4,22 @@ $( document ).ajaxComplete(function() {
 	$( "td:contains('High')" ).css('background-color', '#ff5959' );
 	$( "tr:contains('Unclear')").find('td:first').css('background-color', '#ffcccc' );
 	$( "tr:contains('High')").find('td:first').css('background-color', '#ff5959' );
+	//summary judgment
+	var ratio = 0;
+	var denom = $("tr:contains('PMID')").length;
+	$("#answer").html('Low risk')
+	ratio = $("tr:contains('High')").length/denom;
+	if (ratio > 0.5){
+		$("#judgment").html('High risk');
+		$("#judgment").css('background-color','#ffcccc');
+		$("#rationale").html("\'Most information (" + ratio.toFixed(2) + ") is from studies at low or unclear risk of bias.\' (<a href=\'http://handbook.cochrane.org/chapter_8/table_8_7_a_possible_approach_for_summary_assessments_of_the.htm\'>Cochrane Handbook</a>)");
+	};
+	ratio = $("tr:contains('Unclear')").length/denom;
+	if (ratio > 0.5){
+		$("#judgment").html('High risk');
+		$("#judgment").css('background-color','#ff5959');
+		$("#rationale").html("\'The proportion of information from studies at high risk of bias (<span style='color:red;font-weight:bold'>" + ratio.toFixed(2) + "</span>) is sufficient to affect the interpretation of results.\' (<a href=\'http://handbook.cochrane.org/chapter_8/table_8_7_a_possible_approach_for_summary_assessments_of_the.htm\'>Cochrane Handbook</a>)");
+	};
 });
 $(document).ready(function(){
 $("#header_bias").html("<table><caption>Possible bias in randomized controlled trials of this topic</caption><tr><th class='col1' rowspan='2'>Trial</th><th colspan='2'>Selection bias</th><th>Performance bias</th><th>Detection bias</th><th>Attrition bias</th><th>Reporting bias</th><th>Other biased</th><th rowspan=2 style='width:7px;background-color:white;border: 1px solid white'></th></tr><tr><th>Random sequence generation</th><th>Allocation concealment</th><th>Blinding of participants and personnel</th><th>Blinding of outcome assessment</th><th>Incomplete outcome data</th><th>Selective reporting</th><th>E.g. imbalanced compliance , co-interventions, or other.</th></tr></table>");
