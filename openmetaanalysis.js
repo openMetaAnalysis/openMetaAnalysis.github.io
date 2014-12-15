0d8e07939cdd777d117a692d08281f13f9decfa7
Robert Badgett <rbadgett@kumc.edu>
2014-12-15 05:54:28 -0600

    Corrected pagename

--- openmetaanalysis.js
+++ openmetaanalysis.js
@@ -1,7 +1,7 @@
 var n = location.pathname.indexOf("/",1)
 if (n == 0){n = location.pathname.length}
 var pagename = location.pathname.split('/').slice(-1);
-if (pagename == ""){pagename = "indexhtml"};
+if (pagename == ""){pagename = "index.html"};
 var repo_dir = location.pathname.substring(1,n);
 var repo_name = repo_dir.replace(/\-/gi, ' '); 
 $(document).ready(function(){
