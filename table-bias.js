ccbe357de02d77a4a9b599aee47441c14c84fee7
Robert Badgett <rbadgett@kumc.edu>
2014-12-23 16:33:56 -0600

    Update table-bias.js

--- table-bias.js
+++ table-bias.js
@@ -60,7 +60,7 @@ var url = "/" + repo_dir + "/tables/bias.xml";
 			$("#judgment").html('Low risk')
 			//ratio = $(xml).find("study:contains(High)").length/denom;
 			$("#rationale").html("\'Most information (<span style='color:red;font-weight:bold'>" + eval(100*(1-(highrisksubjects+unclearrisksubjects)/totalsubjects)).toFixed(1) + "% of patients</span>) is from studies at low risk of bias.\' (<a href=\'http://handbook.cochrane.org/chapter_8/table_8_7_a_possible_approach_for_summary_assessments_of_the.htm\'>Cochrane Handbook</a>)");
-			alert((1-highrisksubjects)/totalsubject)
+			alert((1-highrisksubjects)/totalsubjects)
 			if ((1-highrisksubjects)/totalsubjects > 0.5){
 				$("#judgment").html('Unclear risk');
 				$("#judgment").css('background-color','#ffcccc');
