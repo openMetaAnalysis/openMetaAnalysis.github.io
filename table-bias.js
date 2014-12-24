269dfcf5d00527fca35dc08227bfbbf16eb8ea44
Robert Badgett <rbadgett@kumc.edu>
2014-12-23 22:13:37 -0600

    Update table-bias.js

--- table-bias.js
+++ table-bias.js
@@ -60,7 +60,7 @@ var url = "/" + repo_dir + "/tables/bias.xml";
 			$("#judgment").html('Low risk')
 			//ratio = $(xml).find("study:contains(High)").length/denom;
 			$("#rationale").html("\'Most information (<span style='color:red;font-weight:bold'>" + eval(100*(1-(highrisksubjects+unclearrisksubjects)/totalsubjects)).toFixed(1) + "% of patients</span>) is from studies at low risk of bias.\' (<a href=\'http://handbook.cochrane.org/chapter_8/table_8_7_a_possible_approach_for_summary_assessments_of_the.htm\'>Cochrane Handbook</a>)");
-			//alert(1-highrisksubjects/totalsubjects)
+			alert("Non-high risk proportion:\n" + eval(1-highrisksubjects/totalsubjects).toFixed(1))
 			if ((unclearrisksubjects+highrisksubjects)/totalsubjects > 0.5){
 			//Below is per Cochrane, but does not seem sensible as a study could have most trials low risk and also have most trials low or unclear.
 			//if (1-highrisksubjects/totalsubjects > 0.5){
@@ -69,11 +69,11 @@ var url = "/" + repo_dir + "/tables/bias.xml";
 				$("#rationale").html("\'Most information (<span style='color:red;font-weight:bold'>" + eval(100*(unclearrisksubjects+highrisksubjects)/totalsubjects).toFixed(1) + "% of patients</span>) is from studies at unclear or high risk of bias.\' (modified from <a href=\'http://handbook.cochrane.org/chapter_8/table_8_7_a_possible_approach_for_summary_assessments_of_the.htm\'>Cochrane Handbook</a>)");
 			};
 			//ratio = $(xml).find("study:contains(Unclear)").length/denom;
-			//alert(highrisksubjects/totalsubjects)
+			alert("High risk proportion:\n" + eval(highrisksubjects/totalsubjects).toFixed(1))
 			if (highrisksubjects/totalsubjects > 0.5){
 				$("#judgment").html('High risk');
 				$("#judgment").css('background-color','#ff5959');
-				$("#rationale").html("\'The proportion of information (<span style='color:red;font-weight:bold'>" + eval(100*highrisksubjects/totalsubjects).toFixed(2) + "% of patients</span> from studies at high risk of bias is sufficient to affect the interpretation of results.\' (<a href=\'http://handbook.cochrane.org/chapter_8/table_8_7_a_possible_approach_for_summary_assessments_of_the.htm\'>Cochrane Handbook</a>)");
+				$("#rationale").html("\'The proportion of information (<span style='color:red;font-weight:bold'>" + eval(100*highrisksubjects/totalsubjects).toFixed(1) + "% of patients</span> from studies at high risk of bias is sufficient to affect the interpretation of results.\' (<a href=\'http://handbook.cochrane.org/chapter_8/table_8_7_a_possible_approach_for_summary_assessments_of_the.htm\'>Cochrane Handbook</a>)");
 			};
 
                 }
