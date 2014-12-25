79f64314196b7f33e4d3ba5cffb87d8c74a47547
Robert Badgett <rbadgett@kumc.edu>
2014-12-25 16:53:39 -0600

    Update openmetaanalysis.js

--- openmetaanalysis.js
+++ openmetaanalysis.js
@@ -36,8 +36,7 @@ $(document).ready(function(){
 		//$(this).attr('href', "http://www.ncbi.nlm.nih.gov/pubmed/" + $(this).text());
 		$(this).replaceWith($("<a href=\"http://pubmed.gov/" + $(this).text() + "\">" + $(this).text() + '</a>'));
 		})
-	var replaced_text;
-	var str = $("#references").html();
+	var replaced_text = $("#references").html();
 	// Set the regex string for PMCIDs
 	var regex = /(\s{1,})(pmc\d{7,})/ig;
 	// Replace plain text links by hyperlinks
@@ -45,7 +44,7 @@ $(document).ready(function(){
 	// Set the regex string for NCTs
 	regex = /(\s{1,})(NCT\d{7,})/ig;
 	// Replace plain text links by hyperlinks
-	replaced_text = str.replace(regex, "$1<a href='https://clinicaltrials.gov/ct2/show/study/$2'>$2</a>");
+	replaced_text = replaced_text.replace(regex, "$1<a href='https://clinicaltrials.gov/ct2/show/study/$2'>$2</a>");
 	// Set the regex string for PMIDs
 	regex = /(\s{1,})(\d{7,})/ig;
 	// Replace plain text links by hyperlinks
