82d36a389875946e53349705a23e89f50cd1be84
Robert Badgett <rbadgett@kumc.edu>
2015-01-28 08:33:36 -0600

    Update table-bias.js

--- table-bias.js
+++ table-bias.js
@@ -142,7 +142,6 @@ var url = "/" + repo_dir + "/tables/bias.xml";
 	//Reuse
 	$("#business-bias").append("<div style='text-align:center'><a href='https://github.com/openMetaAnalysis/openMetaAnalysis.github.io/blob/master/reusing.MD'>Cite &amp; use this content</a></div>")
 	//Edit and history
-	$("#business-bias").append("<div style='text-align:center'><a href='https://github.com/openMetaAnalysis/" + repo_dir + "/blob/gh-pages/tables/bias.xml'>Edit this page</a> (Hint: use <a href=\"https://kobra.io\">Kobra</a> for collaborative editing) - <a href='https://github.com/openMetaAnalysis/" + repo_dir + "/commits/gh-pages/tables/bias.xml'>History</a></div>")
 	$("#business-bias").append("<div style='text-align:center'>Source content: <a href='/" + repo_dir + "/tables/bias.xml'>view</a> - <a href='https://github.com/openMetaAnalysis/" + repo_dir + "/commits/gh-pages/tables/bias.xml'>history</a> - <a href='https://github.com/openMetaAnalysis/" + repo_dir + "/blob/gh-pages/tables/bias.xml'>edit</a> (Hint: use <a href=\"https://kobra.io\">Kobra</a> for collaborative editing)</div>")	
 	//Issues and comments
 	$("#business-bias").append("<div style='text-align:center'><a href='https://github.com/openMetaAnalysis/" + repo_dir + "/issues?q=is%3Aopen+is%3Aissue'>Issues and comments</a></div>")
