5bf3960ad48802de3f2995aeb88a060b6300d27c
Robert Badgett <rbadgett@kumc.edu>
2015-03-19 22:57:15 -0500

    Support for www.anzctr.org.au

--- table-pico.js
+++ table-pico.js
@@ -27,6 +27,9 @@ var url = "/" + repo_dir + "/tables/pico.xml";
 					if ( $(this).find('citation').attr('trialregistration').toLowerCase().indexOf("isrctn") >= 0){
 						citationtext += "<a href='http://www.isrctn.com/" + $(this).find('citation').attr('trialregistration') + "'>" + $(this).find('citation').attr('trialregistration') + "</a><br>"
 						}
+					if ( $(this).find('citation').attr('trialregistration').toLowerCase().indexOf("actrn") >= 0){
+						citationtext += "<a href='http://www.anzctr.org.au" + $(this).find('citation').attr('trialregistration') + "'>" + $(this).find('citation').attr('trialregistration') + "</a><br>"
+						}
 					}
 				var patients = $(this).find('patients').attr('total') + ' subjects with ' + $(this).find('patients').find('description').text();
 					$(this).find('patients').find('bullet').each(function(){
