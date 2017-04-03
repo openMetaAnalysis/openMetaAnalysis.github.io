438b026830c2990439eec021a70a159c7d458a6d
Robert Badgett <rbadgett@kumc.edu>
2017-04-02 20:23:23 -0500

    First column width

--- table-pico.js
+++ table-pico.js
@@ -2,7 +2,7 @@ $( document ).ajaxComplete(function() {
 	//Background-color of cells
 	});
 $(document).ready(function(){
-$("#header_pico").html("<p>Included trials, starting with the oldest trials.</p><table><caption>Randomized controlled trials of this topic</caption><tbody><tr><th>Trial</th><th>Patients</th><th>Intervention</th><th>Comparison</th><th>Outcome</th><th style='width:7px;background-color:white;border: 1px solid white'></th></tr></table>");
+$("#header_pico").html("<p>Included trials, starting with the oldest trials.</p><table><caption>Randomized controlled trials of this topic</caption><tbody><tr><th width=\'180px\'>Trial</th><th>Patients</th><th>Intervention</th><th>Comparison</th><th>Outcome</th><th style='width:7px;background-color:white;border: 1px solid white'></th></tr></table>");
 var url = "/" + repo_dir + "/tables/pico.xml";
         $.ajax({
             type: "GET",
