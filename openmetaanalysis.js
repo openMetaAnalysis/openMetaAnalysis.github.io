3265509f3bf25cf4ccb13d3cbac0d4b31c262b4a
Robert Badgett <rbadgett@kumc.edu>
2017-10-01 14:52:32 -0500

    Update openmetaanalysis.js

--- openmetaanalysis.js
+++ openmetaanalysis.js
@@ -145,7 +145,7 @@ $(document).ready(function(){
 		var replaced_text = $("#references").html();
 		//alert("Testing:\n\n" + $("#references").html())
 		//remove citation number and colon placed by PubMed
-		var regex = /(\n|\r)\d{1,3}\:\s/g;
+		var regex = /(\n|\r)\d{1,}\:\s/g;
 		replaced_text = replaced_text.replace(regex, "\r");
 		//remove line feeds and replace with space
 		regex = /\r?\n|\r/g
